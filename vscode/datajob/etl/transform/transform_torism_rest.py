d57da357c22c06f2167374b35664ddeb29067f94
Beigee <dbtmdwhd17@gmail.com>
2022-10-25 14:56:01 +0900

    subway & float_pop transform

--- vscode/datajob/etl/transform/transform_torism_rest.py
+++ vscode/datajob/etl/transform/transform_torism_rest.py
@@ -1,15 +1,15 @@
-from infra.jdbc import DataWarehouse, save_data
-from pyspark.sql import Row
+from infra.jdbc import DataWarehouse, find_data, save_data
 from infra.spark_session import get_spark_session
-from infra.util import cal_std_day
-from pyspark.sql.functions import col, count
+from pyspark.sql.functions import *
 
-class TourismTransformRest:
+class TourismRestTransformt:
 
     @classmethod
     def transform(cls):
-        rest = get_spark_session().read.csv('/final_data/tour_attr/강남구_맛집.csv', encoding='CP949', header=True)
-        cols = "순위",'합산 검색 수'
-        rest = rest.drop(*cols)
-        rest
-        # save_data(DataWarehouse, area_pop_fac, 'LOC')
+        sub = find_data(DataWarehouse,'SUBWAY')
+        rest = get_spark_session().read.csv('/final_data/subway/서울시_맛집.csv', encoding='CP949', header=True)
+        tourism = rest.join(sub, on = 'STATION_NAME').select('NAME',col('ADDRESS_1').alias('ADDRESS'),'CATE','SUBCATE'
+                                                                ,col('LAT_1').cast('float').alias('LAT'),col('LON_1').cast('float').alias('LON'),'S_IDX')
+        # tourism.show()
+
+        save_data(DataWarehouse, tourism, 'TOURISM')
